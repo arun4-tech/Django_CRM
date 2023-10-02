b728c9e51cb9b1a33370836de40151e01f0806a7
beegaren <itzmearungeorge@gmail.com>
2023-10-02 20:21:56 +0530

    registration ui done

--- website/views.py
+++ website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+  records= Record.objects.all()
+    
   #Check to see if loggin in
   if request.method == 'POST':
      uname = request.POST['username']
@@ -18,7 +21,7 @@ def home(request):
       messages.success(request,"There was a error Logging In, Please Try Again...")
       return redirect('home')
   else:
-     return render(request, 'home.html',{})
+     return render(request, 'home.html',{'records':records})
 
 
 def logout_user(request):
@@ -41,3 +44,7 @@ def register_user(request):
 	else:
 		form = SignUpForm()
 		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+
+     
